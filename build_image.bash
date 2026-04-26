6a2e9d784f2ddd4756aa0a7ef4e730cfd89ca5ed
K4NEI <kimyoungho0415@gmail.com>
2026-04-26 10:07:30 +0900

    .

--- build_image.bash
+++ build_image.bash
@@ -1,6 +1,6 @@
-podman build -t local/api-gateway:latest -f ./api-gateway/Containerfile
-podman build -t local/discovery-server:latest -f ./discovery-server/Containerfile
-podman build -t local/inventory-service:latest -f ./inventory-service/Containerfile
-podman build -t local/notification-server:latest -f ./notification-server/Containerfile
-podman build -t local/order-service:latest -f ./order-service/Containerfile
-podman build -t local/product-service:latest -f ./product-service/Containerfile
+podman build -t local/api-gateway:latest --label="project=msa-playground" -f ./api-gateway/Containerfile
+podman build -t local/discovery-server:latest --label="project=msa-playground" -f ./discovery-server/Containerfile
+podman build -t local/inventory-service:latest --label="project=msa-playground" -f ./inventory-service/Containerfile
+podman build -t local/notification-server:latest --label="project=msa-playground" -f ./notification-server/Containerfile
+podman build -t local/order-service:latest --label="project=msa-playground" -f ./order-service/Containerfile
+podman build -t local/product-service:latest --label="project=msa-playground" -f ./product-service/Containerfile
