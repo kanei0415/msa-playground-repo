9b4e7b7d8995841c888512cf4cfc619de26e7aad
K4NEI <kimyoungho0415@gmail.com>
2026-04-26 10:59:49 +0900

    .

--- clean.bash
+++ clean.bash
@@ -1,3 +1,3 @@
-podman stop $(podman ps -q -f "label=project=msa-playground") && \
-  podman rm $(podman ps -a -q -f "label=project=msa-playground") && \
-  podman rmi $(podman images -q -f "label=project=msa-playground")
+podman stop $(podman ps -aq -f "label=project=msa-playground") 2>/dev/null || true && \
+podman rm $(podman ps -aq -f "label=project=msa-playground") 2>/dev/null || true && \
+podman rmi $(podman images -q -f "label=project=msa-playground") 2>/dev/null || true
